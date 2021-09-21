eb5ac777aad8c2104b4470fc05decb5644ea5b72
janeventer <janeanneventer@gmail.com>
2021-09-21 11:33:01 -0700

    display images with dATE AND TITLE'

--- src/Images/Images.js
+++ src/Images/Images.js
@@ -2,16 +2,25 @@ import React, { useState, useEffect }  from 'react';
 
 export default function Images({data}){
 
-    const [images, setImages] = useState(data)
+    const [images, setImages] = useState(data.images)
 
     useEffect( () => {
-        console.log('images: ' + images.url)
+        console.log('images: ' + images)
     })
     return(
         
         <div className="Images">
             <h2>Images In Space</h2>
-            <img src={images.url}></img>
+            {images.forEach( (image, ind) => {
+                <div className="image" id={ind}>
+
+                    <img src={image.hdurl}></img>
+                    <p>Date: {image.date}</p>
+                    <p>Title: {image.title}</p>
+                    
+                </div>
+            } )}
+            
         </div>
         
     )
