d48b85174c4e84e798f6a2a38884061a054ce4fe
janeventer <janeanneventer@gmail.com>
2021-09-21 12:54:24 -0700

    add spacing, styling, and color

--- src/SignInComponent/SignInComponent.js
+++ src/SignInComponent/SignInComponent.js
@@ -5,11 +5,11 @@ export default function SignInComponent({loggedIn, onSignIn}){
     return(
         
         <form onSubmit={onSignIn}>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="email"></input>
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="email"></input>
-            <button type="submit">Submit</button>
+            <label htmlFor="name" > Name </label>
+            <input type="text" id="name" name="name" placeholder="your name"></input>
+            <label htmlFor="email"> Email </label>
+            <input type="text" id="email" name="email"placeholder="your email"></input>
+            <button type="submit"> Submit </button>
         </form>
         
     
