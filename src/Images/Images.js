7a9c9735d769641144dac4cd2d6da7f6a365f4d6
janeventer <janeanneventer@gmail.com>
2021-09-21 16:44:38 -0700

    place image and captions inside an unordered list

--- src/Images/Images.js
+++ src/Images/Images.js
@@ -7,29 +7,29 @@ export default function Images({data}){
     const [images, setImages] = useState(data.images)
 
     useEffect( () => {
-        console.log('images: ' + images[0] +images[0].hdurl + (images[0].date).toString() )
+        //console.log('images: ' + images[0] +images[0].hdurl + (images[0].date).toString() )
     })
     return(
         
-        <div className="Images">
+        <ul className="Images">
             
             {
             images.map( (image, ind) => {
                 return(
-                <div className="image" id={ind}>
+                <li className="image" key={ind}>
 
                     <img src={image.hdurl}></img>
                     <footer>Date: {image.date.toString()}</footer>
                     <footer>Title: {JSON.stringify(image.title) }</footer>
                     <LikeButton id={ind} ></LikeButton>
                     <hr></hr>
-                </div>
+                </li>
                 )
             } )
             
             }
             
-        </div>
+        </ul>
         
     )
 
