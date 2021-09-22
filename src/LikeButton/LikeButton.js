3817c56a22942c9e6adacdc99aa6ca2a31fb6e02
janeventer <janeanneventer@gmail.com>
2021-09-21 17:01:56 -0700

    add star giph and yellow color to "liked"

--- src/LikeButton/LikeButton.js
+++ src/LikeButton/LikeButton.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import './LikeButton.css';
 
 export default function LikeButton({ind}){
     const [liked, setLiked] = useState(false);
@@ -8,15 +9,20 @@ export default function LikeButton({ind}){
         setLiked(newLiked)
     }
 
-    useEffect( () => {
+    //useEffect( () => {
         
-    })
+    //})
     return(
         <div className="LikeButton">
-            <button className="likebutton" id={ind} onClick={onClickButton} value={liked? "unlike" : "like"}></button>
+            <button className="likebutton" id={ind} onClick={onClickButton} value={liked? "unlike" : "like"}>
+                {liked? "unlike" : "like"}
+            </button>
             {
                 liked ?
-                <p>Liked!</p>
+                <div>
+                    <p className="liked">Liked!</p>
+                    <img className="star" name="star" src="https://media.giphy.com/media/k9F6ZtOTEr4UGmt3H2/giphy.gif" />
+                </div>
                 :
                 ""
             }
