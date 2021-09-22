f84dbe74ec4c945422199836c0b792b973b853ac
janeventer <janeanneventer@gmail.com>
2021-09-21 17:12:58 -0700

    control form input

--- src/SignInComponent/SignInComponent.js
+++ src/SignInComponent/SignInComponent.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignInComponent.css';
 
 export default function SignInComponent({loggedIn, onSignIn}){
+    const [formData, setFormData] = useState({name: "", email: ""})
+
+    const onChangeForm = (event) => {
+        
+        console.log('event' + event.target.name)
+        const {name, value} = event.target
+        setFormData({...formData, ...{[name]: value}})
+    }
     return(
         
         <form className="SignInComponent" onSubmit={onSignIn}>
             <label htmlFor="name" > Name </label>
-            <input type="text" id="name" name="name" placeholder="your name"></input>
+            <input type="text" id="name" name="name" onChange={onChangeForm} placeholder="your name" value={formData.name}></input>
             <label htmlFor="email"> Email </label>
-            <input type="text" id="email" name="email"placeholder="your email"></input>
+            <input type="text" id="email" name="email" onChange={onChangeForm} placeholder="your email" value={formData.email}></input>
             <button type="submit"> Submit </button>
         </form>
         
