d2885dbb5b3b5a7f94cd0e67dd97226b3205e097
janeventer <janeanneventer@gmail.com>
2022-01-17 16:55:31 -0800

    add styling, including borders and hover effects

--- src/SignInComponent/SignInComponent.js
+++ src/SignInComponent/SignInComponent.js
@@ -11,13 +11,19 @@ export default function SignInComponent({loggedIn, onSignIn}){
     return(
         
         <form className="SignInComponent" onSubmit={onSignIn}>
+
+        <div className="name-container">
             <label htmlFor="name" > Name </label>
             <input type="text" id="name" name="name" onChange={onChangeForm} placeholder="your name" value={formData.name}></input>
+        </div>
+        <div className="email-container">
             <label htmlFor="email"> Email </label>
             <input type="text" id="email" name="email" onChange={onChangeForm} placeholder="your email" value={formData.email}></input>
-            <button type="submit"> Submit </button>
+        </div>
+        <div className="submit-container">
+            <button type="submit" className="submit-button"> Log In</button>
+        </div>
+
         </form>
-        
-    
     )
 }
